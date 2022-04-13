6dcaa1390d24a23e2646284b0eec75e645d7379f
yubo <yubo@yubo.org>
2022-04-13 21:42:54 +0800

    add periodtime

---
+++ util/periodtime.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"time"
+)
+
+func NewPeriodTime(period time.Duration, min, max float64) *PeriodTime {
+	return &PeriodTime{
+		period: period.Nanoseconds(),
+		min:    min,
+		max:    max,
+	}
+}
+
+type PeriodTime struct {
+	period time.Duration
+	min    float64
+	max    float64
+}
+
+func (p *PeriodTime) Time(t time.Time) float64 {
+	return (float64((t.UnixNano())%p.period)/float64(p.period))*(p.max-p.min) + p.min
+}
